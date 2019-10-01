5d6cc13cd37417992b9d3a3dbc3ff97c1c0368f9
Mohsen Ali Mohsen <muhsin86@hotmail.se>
2019-10-01 23:51:51 +0200

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,83 @@
-const { src, dest, watch, series, parallel } = require('gulp'),
-concat = require('gulp-concat'),
-cssnano = require('gulp-cssnano'),
-autoprefixer = require('gulp-autoprefixer'),
-uglifyes = require('gulp-uglify-es').default,
-imagemin = require('gulp-imagemin'),
-sourcemaps = require('gulp-sourcemaps'),
-sass = require('gulp-sass'),
-rename = require('gulp-rename'),
+const { src, dest, watch, series, parallel } = require("gulp");
+concat = require("gulp-concat");
+concatCss = require('gulp-concat-css');
+cssnano = require('gulp-cssnano');
+autoprefixer = require('gulp-autoprefixer');
+uglifyes = require('gulp-uglify-es').default;
+sass = require('gulp-sass');
+sassComplier = require('node-sass');
+browserSync = require('browser-sync').create();
 livereload = require('gulp-livereload');
 
-// Paths
-const files = {
-    htmlPath: "src/**/*.html",
-    jsPath: "src/**/*.js",
-    scssPath: "src/**/*.scss",
-    cssPath: "public/**/*.css",
-    imagePath: "src/images/*"
-}
+const files = {
+	htmlPath: 'src/**/*.html',
+	cssPath: "src/**/*.css",
+	scssPath: "src/**/*.scss",
+	jsPath: "src/**/*.js", 
+	imagePath: "src/images/*"
+	
+};
 
-// Tasks for copying a html files and images
-function html() {
-    return src(files.htmlPath)
-    .pipe(dest('public'))
-    .pipe(livereload());
+function html() {
+    return src(files.htmlPath)
+        .pipe(dest('Public'))
+		.pipe(browserSync.stream())
+		.pipe(livereload());  
 }
-
-function image() {
-        return src(files.imagePath)
-        .pipe(imagemin())
-        .pipe(dest('public/images'))
-        .pipe(livereload());
-    }
-    
 
 // Tasks for Concatenating And Minifying JavaScript and CSS Files
 
 function js() {
-    return src(files.jsPath)
-    .pipe(concat('main.js'))
-    .pipe(uglifyes())
-    .pipe(dest('public/js'))
-    .pipe(livereload()); 
+	    return src(files.jsPath)
+	    .pipe(concat('main.js'))
+	    .pipe(uglifyes())
+	    .pipe(dest('public/js'))
+	    .pipe(livereload()); 
 }
 
-function css() {
-    return src(files.cssPath)
-    .pipe(autoprefixer())
-    .pipe(concat('main.css'))
-    .pipe(concat('style.css'))
+function css()
+{
+	return src(files.cssPath, files.scssPath)
+	.pipe(concatCss('main.css'))
+	.pipe(concatCss('style.css'))
+	.pipe(autoprefixer())
     .pipe(cssnano())
-    .pipe(dest('public/css/sass'))
-    .pipe(livereload());   
+	.pipe(dest('Public/css'))
+	.pipe(browserSync.stream())
+	.pipe(livereload());  
+}
+
+function scss()
+{
+	return src(files.scssPath)
+	.pipe(sass().on('error', sass.logError))
+	.pipe(dest('Public/css'))
+	.pipe(browserSync.stream())
+	.pipe(livereload());  
 }
 
 
-// Task for compile Scss Files
-function scss() {
-  return src(files.scssPath)
-      .pipe(sass().on('error', sass.logError))
-      .pipe(dest('public/css/'))
-      .pipe(livereload()); 
+// Task: Läsa in ImagesPath från files och kopiera bilder från src/Images till pub/Images
+function image()
+{
+	return src(files.imagePath)
+	.pipe(dest('Public/images'))
+	.pipe(livereload());  
 }
 
-// Watching tasks
 
-function watchTask() {
-    livereload.listen();
-    watch([files.htmlPath, files.jsPath, files.cssPath, files.scssPath, files.imagePath],
-       parallel(html, js, css, scss, image))
+function watchTask()
+{
+	livereload.listen();
+	browserSync.init({
+		server:{
+			baseDir: 'Public/' }
+		});
+	watch([files.htmlPath, files.jsPath, files.scssPath, files.cssPath, files.imagePath],
+        parallel(html, js, scss, css, image)
+    ).on('change', browserSync.reload);
 }
 
-// Gulp basic task
-
-exports.default = series(
-    parallel(html, js, css, scss, image),
-    watchTask
+exports.default = series(
+    parallel(html, js, scss, css, image, watchTask),
 );
